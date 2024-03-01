695087ef68a0dec6dceb23545176be105dacdf7a
jorainer <johannes.rainer@gmail.com>
2024-03-01 21:02:07 +0100

    Add color scheme for framework

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 xrdb merge ~/.Xresources 
+
+# /usr/bin/xcalib -d :0 /usr/share/color/icc/colord/BOE_CQ_______NE135FBM_N41_01.icm
+
 # xbacklight -set 10 &
 # feh --bg-fill ~/Pictures/wall/gruv.png &
 # xset r rate 200 50 &
